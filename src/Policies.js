4a2c0d3445fd5296bce692c7509be9264217cc72
Q <Q@Skylars-MacBook-Pro.local>
2020-11-30 07:47:19 -0800

    rushing

---
+++ src/Policies.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Navigation from "../Navigation/Nav";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const PoliciesPage = () => (
+  <body>
+    <Navigation />
+    <Header />
+    <h1>Class Policies</h1>
+    <h2>
+      <span>SOUS CHEF PROGRAM</span>
+    </h2>
+    <p>
+      In the Kitchen offers those who share our passion for cooking an
+      opportunity to earn free classes! After assisting with three classes, you
+      can attend an additional class of your choice for free. Be ready to do the
+      following as a Sous Chef: Arrive at In the Kitchen Cooking School at least
+      one hour before class to help with set-up and be prepared to stay about an
+      hour and a half for clean up after class. Help with mise en place-light
+      prep work, such as washing, chopping and setting up the stations. Round up
+      ingredients for the chef during the class. E-mail us to get started
+      earning classes today!
+    </p>
+    <h2>
+      <span>COOKING CLASS POLICIES</span>
+    </h2>
+    <p>
+      Participation Class For your safety, please wear closed-toed shoes. Each
+      student will help with preparing all the recipes, and then we’ll all sit
+      down to enjoy the delicious dishes we have made. This is a full meal, so
+      come with an appetite! 3 hours 12 people $70/per person Demonstration
+      Class It’s not TV! Demonstration classes are a fun way to learn. You’ll
+      have the best seat In the Kitchen as you watch your chef prepare all the
+      dishes in the menu. You’ll see all the classic techniques, and learn some
+      cool tricks of the pros, as the chef creates a delicious meal. There will
+      be generous samples of each dish. 2-1/2 hours 30 people $60 per person
+    </p>
+    <br />
+    <span>CANCELLATION POLICY</span>
+    <p>
+      Any class canceled between 2 weeks and 3 business days prior to class will
+      be issued a class credit to be used within 1 month of the date of the
+      canceled class. No refunds or credits will be issued for last-minute
+      cancellations (less than 3 business days) or no-shows. You are welcome to
+      send a friend in your place if you must cancel.
+    </p>
+    <span>PRIVATE CLASS POLICY</span>
+    <p>
+      A 50% nonrefundable deposit is required to reserve your date. Your final
+      guest count is needed one week before your event, and the balance is due
+      at this time. Please have your guests arrive no earlier than 1/2 hour
+      before your event, but please also ask that everyone be on time. Private
+      hands-on classes are 3 hours, and demonstration classes are 2-1/2 hours.
+      Longer class times are available, and prices for longer classes will be
+      discussed at the time you book your party.
+    </p>
+    <span> WEATHER POLICY</span>
+   <p> In case of inclement weather, please call us at (609) 206-4511 about your
+    class.</p>
+    <Footer />
+  </body>
+);
+export default PoliciesPage;
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,61 @@ const Home = (props) => {
     return(
          <>
           <h1>Home</h1>
+          <div className="App">
+        <Navigation />
+        <Header />
+        <section>
+          <h1 id="all_about_the_food">It's All About the Food!</h1>
+          <br />
+          <div id="cake">
+            <p id="cake-text">
+              <span>click on it once your done drooling over it</span>
+              <small>
+                Just one of the mouth drooling examples we have here, wanna
+ 
+               learn this recipe? Click<a href="#"> here</a> the link to book a
+                spot on our course schedule
+              </small>
+            </p>
+          </div>
+          <p id="kitchen-goal">
+            Here at In the Kitchen Cooking School, we invite you to join us for
+            a deliciously fun and unique way to spend some time. You’ll want to
+            try all of our wonderful classes, where you will learn new skills,
+            make new friends, have a ball, and dine on the most delectable food
+            around.
+            <br />
+            We have a class for everyone!
+          </p>
+          <br />
+          <h2>Never Cook Anything Else But Toast?</h2>
+          <h2>Is The Microwave Your Best Friend?</h2>
+          <h2>Does Cooking Breakfast Really Mean Pouring Milk In Cereal?</h2>
+          <br />
+          <p>
+            Take a bite out of our World Flavors series and explore a universe
+            of delicious and unique cuisines. Try it all, from French, Italian,
+            Spanish and Mexican, to Vietnamese, Thai, Greek and more.
+            <br />
+            Learn more about our <a href="#">Cooking Classes</a> and{" "}
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+           {/*}  eslint-disable-next-line jsx-a11y/anchor-is-valid  */}
+            <a href="#">Team Building events!</a>
+            <br />
+            If there is something you want to learn that is not already on our
+            menu, In the Kitchen Cooking School will create a class especially
+            for you! So bring your spouse, or a friend, or come meet some new
+            friends and join us for some delicious food and great fun.
+          </p>
+          <br />
+          <h3>Get ready to spice up your life!</h3>
+        </section>
+      
+        
+       
+      </div>
+    </>
          
-          {/* <input id="time" type="text" value="10:00"></input> */}
-          </>
-    )
+    );
 }
 export default withRouter(Home);
